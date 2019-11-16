e93a67bc1dab0cdfe0f1a22729f8add060177c02
fernando <fernando.patricio.maceda@gmail.com>
2019-11-16 14:02:09 -0600

    Error corregido

--- src/main/java/FrmLogin.java
+++ src/main/java/FrmLogin.java
@@ -54,8 +54,6 @@ public class FrmLogin extends javax.swing.JFrame {
         TxtContrasena = new javax.swing.JPasswordField();
         BtnCancelar = new javax.swing.JButton();
         BtnIngresar = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
-        TxtRango = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -91,14 +89,6 @@ public class FrmLogin extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setText("Rango");
-
-        TxtRango.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TxtRangoActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -111,15 +101,13 @@ public class FrmLogin extends javax.swing.JFrame {
                             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
                             .addComponent(TxtUsuario, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
-                            .addComponent(TxtContrasena)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE)
-                            .addComponent(TxtRango, javax.swing.GroupLayout.DEFAULT_SIZE, 175, Short.MAX_VALUE))
+                            .addComponent(TxtContrasena))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(BtnCancelar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 124, Short.MAX_VALUE)
                         .addComponent(BtnIngresar)
-                        .addGap(53, 53, 53))))
+                        .addGap(54, 54, 54))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -132,15 +120,15 @@ public class FrmLogin extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(TxtContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(TxtRango, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(BtnCancelar)
-                    .addComponent(BtnIngresar))
-                .addGap(20, 20, 20))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(BtnCancelar)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
+                        .addComponent(BtnIngresar)
+                        .addGap(71, 71, 71))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -195,12 +183,12 @@ public class FrmLogin extends javax.swing.JFrame {
             }
             
             intentos++;
-            rango = TxtRango.getText();
+            
             usu = TxtUsuario.getText();
             con = TxtContrasena.getText();
             
             Seguridad s = new Seguridad();
-            s.validarUsuario(usuarios, usu, con, rango, intentos);
+            s.validarUsuario(usuarios, usu, con, intentos);
             
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
@@ -221,10 +209,6 @@ public class FrmLogin extends javax.swing.JFrame {
     private void TxtContrasenaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtContrasenaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_TxtContrasenaActionPerformed
-
-    private void TxtRangoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtRangoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_TxtRangoActionPerformed
 
     private void BtnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnCancelarActionPerformed
         // TODO add your handling code here:
@@ -270,11 +254,9 @@ public class FrmLogin extends javax.swing.JFrame {
     private javax.swing.JButton BtnCancelar;
     private javax.swing.JButton BtnIngresar;
     private javax.swing.JPasswordField TxtContrasena;
-    private javax.swing.JTextField TxtRango;
     private javax.swing.JTextField TxtUsuario;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/main/java/Seguridad.java
+++ src/main/java/Seguridad.java
@@ -14,25 +14,28 @@ import javax.swing.JOptionPane;
 public class Seguridad {
     FrmLogin login = new FrmLogin();
     String res;
-    public void validarUsuario(String usuario[], String usu, String con, String rango, int intentos){
+    String ran = "Administrador"; 
+    String ran1 = "Vendedor";
+    
+   public void validarUsuario(String usuario[], String usu, String con, int intentos){
         boolean encontrado = false;
         for (int i = 0; i < usuario.length; i++) {
-            if(usuario[i].equalsIgnoreCase(usu) && usuario[i+1].equals(con) && usuario[i+2].equalsIgnoreCase(rango)){
+            if(usuario[i].equalsIgnoreCase(usu) && usuario[i+1].equals(con) && usuario[i+2].equals(ran)){
                 res = "Bienvenido " + usu;
                 encontrado = true;
                 JOptionPane.showMessageDialog(null, res, " Inicio de sesion", JOptionPane.INFORMATION_MESSAGE);
                 intentos = 0;
                 login.setIntentos(intentos);
-                // una vez el usuiario entre, se abre el formulario padre
+                // una vez el usuiario entre, se abre el formulario administrador
                 new FrmInicioAdm().setVisible(true);
                 break;
-            }else if(usuario[i].equalsIgnoreCase(usu) && usuario[i+1].equals(con) && usuario[i+2].equalsIgnoreCase(rango)){
+            }else if(usuario[i].equalsIgnoreCase(usu) && usuario[i+1].equals(con) && usuario[i+2].equals(ran1)){
                 res = "Bienvenido " + usu;
                 encontrado = true;
                 JOptionPane.showMessageDialog(null, res, " Inicio de sesion", JOptionPane.INFORMATION_MESSAGE);
                 intentos = 0;
                 login.setIntentos(intentos);
-                // una vez el usuiario entre, se abre el formulario padre
+                // una vez el usuiario entre, se abre el formulario ventas
                 new FrmInicioVen().setVisible(true);
             }
         }
